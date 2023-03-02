c105861f2c523c9b35a8ce291caca9a3e81cf2ae
Ih-Vshch <igvashch@gmail.com>
2023-03-02 11:55:33 +0200

    react added

---
+++ src/data.js
@@ -0,0 +1,71 @@
+export const data = {
+  fullName: "Ihor Vashchenko",
+  position: "FRONT-END WEB DEVELOPER",
+  summary: [
+    "Self-motivated and hard-working individual with strong analytical skills desires to work as a Front-end Web Developer",
+    "Bringing 1year non-comercial programming experience in JavaScript, HTML, CSS/SCSS and pro iciency in relevant packages such as React, Redux toolkit, Next",
+    "Also has good knowledges in CSS frameworks - Bootstrap, Material UI; backend and database technologies - Node, Express, MongoDB, Mongoose; programming and software tools - Docker, Git",
+  ],
+  skills: [
+    "React",
+    "Redux",
+    "toolkit",
+    "JavaScript (+ES6-11)",
+    "HTML",
+    "CSS",
+    "SCSS",
+    "Next",
+    "Material UI",
+    "Bootstrap",
+    "MongoDB",
+    "Mongoose",
+    "Node",
+    "Express",
+    "Docker",
+    "Docker-compose",
+    "Git",
+  ],
+  keyProjects: [
+    {
+      title: "Transport ticket service",
+      position: "Co-Developer",
+      period: "01/2022 - present",
+      description:
+        "Continued development new components, refactoring code for hooks. Used - React, Redux, Next, Axios, SCSS",
+    },
+    {
+      title: "Grant fund application processing",
+      position: "Developer",
+      period: "10/2020 - 01/2021",
+      description:
+        "Developed web-app based on Google App Script for processing information on applications to the grant fund (submission, editing, search). Used for frontend - HTML, JS, CSS (Bootstrap), for data repository - Google Spreadsheets",
+    },
+  ],
+  workExperience: [
+    {
+      title: "Experience in civil engineering and construction industry",
+      position:
+        "Expert in energy ef iciency, Chief project engineer, Design engineer",
+      period: "03/2011 - 05/2022",
+      description:
+        "Design of heating, ventilation, cooling, water and gas supply system, implementation of energy ef iciency measures for industrial and civil buildings, transport infrastructure",
+    },
+  ],
+  courses: [
+    {
+      company: "Beetroot Academy",
+      name: '"Advanced JavaScript/React "',
+      period: "2021",
+    },
+  ],
+  education: [
+    {
+      university:
+        "Kharkiv National University of Civil Engineering and Architecture",
+      degree: "Master’s degree",
+      specialty: "“Heat and gas supply and ventilation”",
+      period: "2005 - 2010",
+    },
+  ],
+  languages: ["Native Ukrainian", "Native Russian", "Intermediate English"],
+};
